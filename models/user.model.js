662b0ad7dc363566c66704f784ed73377ff0a3fa
RomainFauche <91599605+RomainFauche@users.noreply.github.com>
2022-07-10 11:40:32 +0200

    ok

---
+++ models/user.model.js
@@ -0,0 +1,64 @@
+const mongoose = require("mongoose");
+//const { STRING } = require("mysql/lib/protocol/constants/types");
+const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema(
+  {
+    pseudo: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 55,
+      unique: true,
+      trimp: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      validate: [isEmail],
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      max: 1024,
+      minlength: 6,
+    },
+    picture: {
+      type: String,
+      default: "./uploads/profil/ramdom-user.png",
+    },
+    bio: {
+      type: String,
+      max: 2000,
+    },
+    followers: {
+      type: [String],
+    },
+    following: {
+      type: [String],
+    },
+    likes: {
+      type: [String],
+    },
+    dislikes: {
+      type: [String],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// play function before save into display: 'block'
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+const UserModel = mongoose.model("user", userSchema);
+
+module.exports = UserModel;
